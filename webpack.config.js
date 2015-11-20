98da78fa5c5d8f1de72188118855ae2429cff6a9
Peter Murphy <peter71@gmail.com>
2015-11-20 10:02:52 -0800

    Update Pond website

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,40 @@
-var webpack = require('webpack');
+/* eslint "no-var": 0 */
+
+var webpack = require("webpack");
 
 var plugins = [
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-  })
+    new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    })
 ];
 
 if (process.env.COMPRESS) {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    })
-  );
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        })
+    );
 }
 
 module.exports = {
 
-  output: {
-    library: ["ESnet", "Pond"],
-    libraryTarget: 'assign'
-  },
+    output: {
+        library: ["ESnet", "Pond"],
+        libraryTarget: "assign"
+    },
 
-  module: {
-    loaders: [
-      { test: /\.(js|jsx)$/, loader: 'babel?optional=es7.objectRestSpread' }
-    ]
-  },
+    module: {
+        loaders: [
+            { test: /\.(js|jsx)$/,
+              loader: "babel?optional=es7.objectRestSpread" }
+        ]
+    },
 
-  node: {
-    Buffer: false
-  },
+    node: {
+        Buffer: false
+    },
 
-  plugins: plugins
-
+    plugins: plugins
 };
--- website/modules/main.jsx
+++ website/modules/main.jsx
@@ -17,19 +17,14 @@ import Router from "react-router";
 import App from "./app.jsx";
 import Intro from "./intro.jsx";
 import Start from "./start.jsx";
-
 import Time from "./time.jsx";
 import TimeRange from "./timerange.jsx";
 import Index from "./index.jsx";
-
 import Events from "./events.jsx";
-
 import TimeSeries from "./timeseries.jsx";
-
 import Aggregators from "./aggregators.jsx";
 import Collectors from "./collectors.jsx";
 import Binners from "./binners.jsx";
-
 import Rollup from "./rollup.jsx";
 
 const {Route, DefaultRoute} = Router;
@@ -38,9 +33,7 @@ const routes = (
     <Route path="/" handler={App}>
         <DefaultRoute name="intro" handler={Intro} />
         <Route name="start" handler={Start} />
-
         <Route name="rollups" handler={Rollup} />
-
         <Route name="time" handler={Time} />
         <Route name="timerange" handler={TimeRange} />
         <Route name="index" handler={Index} />
