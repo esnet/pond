8bdd7f6d85f30503ecc98c6cbd6099bcda9c17af
Peter Murphy <pmurphy@es.net>
2016-04-21 09:44:03 -0700

    Cleanup dependencies. Create a babelrc file. Print babel version number when transpiring.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,8 @@
 echo "> Start transpiling"
 echo ""
-rm -rf lib/* && ./node_modules/.bin/babel src --presets es2015 --plugins 'transform-runtime' src --out-dir ./lib
+rm -rf lib/*
+./node_modules/.bin/babel --version
+echo ""
+./node_modules/.bin/babel src --plugins 'transform-runtime' src --out-dir ./lib
 echo ""
 echo "> Complete transpiling"
